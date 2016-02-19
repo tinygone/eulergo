885eadad685c0ab97cff8aa5f2ae72f440b8d175
tinygone <tinygone@qq.com>
2016-02-19 21:21:55 +0800

    problem7 resolved

---
+++ src/eulergo/problem7.go
@@ -0,0 +1,34 @@
+package eulergo
+
+import (
+	"fmt"
+	"strconv"
+	//"strings"
+)
+
+func Problem7() {
+	fmt.Println("Project Euler Problem7")
+
+	var primeArray = make([]int64, 0, 0)
+	primeArray = append(primeArray, 2)
+	var destCount = 10001
+	var i int64 = 3
+	for true {
+		if isPrime(i) {
+			primeArray = append(primeArray, i)
+		}
+
+		if len(primeArray)%10 == 0 {
+			//fmt.Println(primeArray)
+		}
+
+		if len(primeArray) > destCount {
+			break
+		}
+		i++
+	}
+	fmt.Println(primeArray)
+
+	fmt.Println("result = " + strconv.FormatInt(i, 10))
+
+}
