e2358f6cfb22628d97be8f8c14f08eaccced312c
tinygone <tinygone@qq.com>
2016-02-20 19:29:38 +0800

    problem10 resolved

---
+++ src/eulergo/problem10.go
@@ -0,0 +1,29 @@
+package eulergo
+
+import (
+	"fmt"
+	"strconv"
+	//"strings"
+)
+
+func Problem10() {
+	fmt.Println("Project Euler Problem10")
+
+	var sum int64 = 0
+
+	primeList := make([]int64, 0, 0)
+	var i int64 = 2
+	for i = 2; i < 2000000; i++ {
+		if isPrime(i) {
+			primeList = append(primeList, i)
+		}
+
+	}
+
+	for _, t := range primeList {
+		sum += t
+	}
+
+	fmt.Println("result = " + strconv.FormatInt(sum, 10))
+
+}
